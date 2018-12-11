ecd094efe5790a1ffcc20579f22ce0e8311de2b6
Ingo Brenckmann <ingo.brenckmann@porschedigitallab.com>
2018-12-11 14:34:39 +0100

    Before and After tests added

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,20 +1,42 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
+    private Greeting greeting;
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before All - Only called once");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Init before each ...");
+        greeting = new Greeting();
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
+        greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void helloWorld1() {
-        Greeting greeting = new Greeting();
+        greeting = new Greeting();
         System.out.println(greeting.helloWorld("Ingo"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Tear down - After Each");
+    }
+
+    @AfterAll
+    public static void afterAllClass() {
+        System.out.println("After All - only called once");
     }
 }
